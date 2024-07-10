f80676294099bb0f7d6b7bb34ca0c4d5e8226c32
AI-arc-gen <architkumar41009@gmail.com>
2024-07-10 19:59:14 +0530

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,31 @@
+import time 
+
+
+def countdown_timer(seconds):
+    
+    
+    
+    while seconds > 0:
+        
+        
+        mins = int(seconds / 60)
+        secs = int(seconds % 60)
+        
+        
+        timer = f'{mins}:{secs}'
+        
+        
+        print(timer)
+        
+        
+        seconds -= 1
+        
+        
+    print('Time Up!')
+    
+    
+
+seconds = input("Enter the time in nuumbers of seconds: ")
+        
+        
+countdown_timer(int(seconds))
